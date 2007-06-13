39760ea6931b520330e5a6bce72323385b0e72ae
mkersten <>
2007-06-13 21:22:57 +0000

    NEW - bug 124321: [api] merge local tasks with repository tasks https://bugs.eclipse.org/bugs/show_bug.cgi?id=124321

--- org.eclipse.mylyn.sandbox.ui/src/org/eclipse/mylyn/internal/sandbox/views/TaskActivityView.java
+++ org.eclipse.mylyn.sandbox.ui/src/org/eclipse/mylyn/internal/sandbox/views/TaskActivityView.java
@@ -37,8 +37,8 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.ViewerDropAdapter;
-import org.eclipse.mylyn.internal.tasks.core.DateRangeActivityDelegate;
-import org.eclipse.mylyn.internal.tasks.core.DateRangeContainer;
+import org.eclipse.mylyn.internal.tasks.core.ScheduledTaskDelegate;
+import org.eclipse.mylyn.internal.tasks.core.ScheduledTaskContainer;
 import org.eclipse.mylyn.internal.tasks.ui.TaskListColorsAndFonts;
 import org.eclipse.mylyn.internal.tasks.ui.actions.ActivityReportAction;
 import org.eclipse.mylyn.internal.tasks.ui.actions.OpenTaskListElementAction;
@@ -140,7 +140,7 @@ public class TaskActivityView extends ViewPart {
 			// TaskActivityView.this.treeViewer.refresh(task);
 		}
 
-		public void activityChanged(DateRangeContainer week) {
+		public void activityChanged(ScheduledTaskContainer week) {
 			refresh();
 			// TaskActivityView.this.treeViewer.refresh(week);
 		}
@@ -319,18 +319,18 @@ public class TaskActivityView extends ViewPart {
 						.getViewer().getSelection());
 
 				Object target = getCurrentTarget();
-				DateRangeContainer container;
+				ScheduledTaskContainer container;
 				Calendar reminderCalendar;
-				if (target instanceof DateRangeContainer) {
-					container = (DateRangeContainer) target;
+				if (target instanceof ScheduledTaskContainer) {
+					container = (ScheduledTaskContainer) target;
 					if (container.isPresent()) {
 						reminderCalendar = GregorianCalendar.getInstance();
 						TasksUiPlugin.getTaskListManager().setSecheduledIn(reminderCalendar, 1);
 					} else {
 						reminderCalendar = container.getStart();
 					}
-				} else if (target instanceof DateRangeActivityDelegate) {
-					DateRangeActivityDelegate dateRangeActivityDelegate = (DateRangeActivityDelegate) target;
+				} else if (target instanceof ScheduledTaskDelegate) {
+					ScheduledTaskDelegate dateRangeActivityDelegate = (ScheduledTaskDelegate) target;
 					if (dateRangeActivityDelegate.getDateRangeContainer().isPresent()) {
 						reminderCalendar = GregorianCalendar.getInstance();
 						TasksUiPlugin.getTaskListManager().setSecheduledIn(reminderCalendar, 1);
@@ -364,11 +364,11 @@ public class TaskActivityView extends ViewPart {
 				}
 
 				Object target = getCurrentTarget();
-				DateRangeContainer dateRangeContainer = null;
-				if (target instanceof DateRangeContainer) {
-					dateRangeContainer = (DateRangeContainer) target;
-				} else if (target instanceof DateRangeActivityDelegate) {
-					DateRangeActivityDelegate dateRangeActivityDelegate = (DateRangeActivityDelegate) target;
+				ScheduledTaskContainer dateRangeContainer = null;
+				if (target instanceof ScheduledTaskContainer) {
+					dateRangeContainer = (ScheduledTaskContainer) target;
+				} else if (target instanceof ScheduledTaskDelegate) {
+					ScheduledTaskDelegate dateRangeActivityDelegate = (ScheduledTaskDelegate) target;
 					dateRangeContainer = dateRangeActivityDelegate.getDateRangeContainer();
 				}
 
@@ -463,8 +463,8 @@ public class TaskActivityView extends ViewPart {
 		reminderEditor.addListener(new ICellEditorListener() {
 			public void applyEditorValue() {
 				Object selection = ((IStructuredSelection) treeViewer.getSelection()).getFirstElement();
-				if (selection instanceof DateRangeActivityDelegate) {
-					DateRangeActivityDelegate dateRangeActivityDelegate = (DateRangeActivityDelegate) selection;
+				if (selection instanceof ScheduledTaskDelegate) {
+					ScheduledTaskDelegate dateRangeActivityDelegate = (ScheduledTaskDelegate) selection;
 					Date newReminder = reminderEditor.getReminderDate();
 					if (newReminder != null) {
 						TasksUiPlugin.getTaskListManager().setScheduledFor(
